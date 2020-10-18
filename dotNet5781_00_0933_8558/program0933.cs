81f59c81bbb8a44d0f15ecf5d099c9a5322f9b26
~Mendy~ B.E <mbembe770@gmail.com>
2020-10-19 00:37:40 +0300

    hello da change

---
+++ dotNet5781_00_0933_8558/program0933.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_0933_8558
+{
+    partial class Program
+    {
+        static partial void Welcome0933()
+        {
+            Console.WriteLine("hello dan");
+        }
+    }
+}
