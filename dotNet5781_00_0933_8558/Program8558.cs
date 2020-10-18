ecfa7eaf87b7f2c8f180d450a024c3e35ccb7039
Shimon Dyskin <shimond111@hotmail.com>
2020-10-19 00:18:51 +0300

    Partial Functions

---
+++ dotNet5781_00_0933_8558/Program8558.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_0933_8558
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8558();
+            Welcome0933();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome0933();
+
+        private static void Welcome8558()
+        {
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
