0925f4ab9735df530e6e8ea7624a91c437777ebf
ShimonD9 <diskin@g.jct.ac.il>
2020-10-25 22:58:20 +0200

    set up solution start up & add new project for targil 1

---
+++ dotNet5781_01_0933_8558/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_0933_8558
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			List<Bus> buses = new List<Bus>();
+			CHOICE choice;
+			bool success;
+			do
+			{
+				Console.WriteLine("enter etc.");
+				string kelet = Console.ReadLine();
+				success = Enum.TryParse(kelet, out choice);
+				if (!success)
+				{
+					Console.WriteLine("Try again"); o
+				}
+			}
+			while (!success);
+			do
+			{
+				switch (choice)
+				{
+					case CHOICE.ADD_BUS:
+						Consoe.WriteLine("ten li pratim");
+						string rishuy;
+						DateTime.taarich;
+						Console.WriteLine("enter date");
+						success = DateTime.TryPrase(Console.ReadLine(), out taarich);
+						Console.WriteLine("teb rishuy");
+						rishuy = Console.ReadLine();
+						if (success)
+						{
+							try
+							{
+								buses.Add(new Bus(taarich, rishuy)); // surround with try
+								foreach (Bus bus in buses)
+								{
+									Console.WriteLine(bus);
+								}
+						catch (Exception exception)
+							{
+								console.WriteLine(exception.Message);
+							}
+						}
+						break;
+				}
+
+			} while (success);
+		}
+    }
+}
