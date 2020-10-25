80c1a2eb008d4b45c8b1ddf906d036a355abcb27
mendi2770 <mbenezra@g.jct.ac.il>
2020-10-25 23:06:27 +0200

    creates Enums

---
+++ dotNet5781_01_0933_8558/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_0933_8558
+{
+    public enum CHOICE
+    {
+        ADD_BUS,PICK_BUS,REFULE_BUS,MAINTENANCE_BUS,EXIT=-1
+    }
+}
