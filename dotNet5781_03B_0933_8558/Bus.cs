6595409b935112669cb7d992d35a404693865a96
mendi2770 <73079004+mendi2770@users.noreply.github.com>
2020-12-08 12:48:57 +0200

    bus.cs NOTES

--- dotNet5781_03B_0933_8558/Bus.cs
+++ dotNet5781_03B_0933_8558/Bus.cs
@@ -20,8 +20,8 @@ namespace dotNet5781_03B_0933_8558
         /// <param name="km" - equals 0 if not initialized by user></param>
         public Bus(string licenseInput, double km, double kmAtLastTreat, DateTime dateEntry, DateTime dateOfLastTreat) // Bus constructor
         {
-            DateOfAbsorption = dateEntry;
-            License = licenseInput;
+            DateOfAbsorption = dateEntry;           //date of starting to work
+            License = licenseInput;             
             KMLeftToTravel = 1200; // Assuming every added bus is filled with gas
             Mileage = km;
             MileageAtLastTreat = kmAtLastTreat;
@@ -41,18 +41,18 @@ namespace dotNet5781_03B_0933_8558
             {
                 // Firstly - cutting the string to 3 parts, and then formatting them into a new one, adding dashes
                 string prefix, middle, suffix, result;
-                if (license.Length == 7)
+                if (license.Length == 7)            //in case the bus is before 2018
                 {
-                    prefix = license.Substring(0, 2);
-                    middle = license.Substring(2, 3);
-                    suffix = license.Substring(5, 2);
+                    prefix = license.Substring(0, 2);       //two first digits
+                    middle = license.Substring(2, 3);       //three middle digits
+                    suffix = license.Substring(5, 2);       //two last digits
                     result = string.Format("{0}-{1}-{2}", prefix, middle, suffix);
                 }
                 else
-                {
-                    prefix = license.Substring(0, 3);
-                    middle = license.Substring(3, 2);
-                    suffix = license.Substring(5, 3);
+                {                                           //in case the bus is after 2018
+                    prefix = license.Substring(0, 3);       //three first digits
+                    middle = license.Substring(3, 2);       //two middle digits
+                    suffix = license.Substring(5, 3);       //three last digits
                     result = string.Format("{0}-{1}-{2}", prefix, middle, suffix);
                 }
                 return result;
@@ -78,43 +78,53 @@ namespace dotNet5781_03B_0933_8558
         }
 
         /////////////////////////////// Status:  ///////////////////////////////
-
+        //in case the status of the bus is "ready for travel"
         public bool IsReady { get { return Status == BUS_STATUS.READY_FOR_TRAVEL; } set { if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs("IsReady")); } } }
-
+        //in case the status of the bus is "dangerous"
         public bool NeedsTreatment { get { if ((Status == BUS_STATUS.DANGEROUS) || (Status == BUS_STATUS.READY_FOR_TRAVEL)) return true; return false; } set { if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs("NeedsTreatment")); } } }
-
+        //in case the status of the bus is "need refuel"
         public bool NeedsRefuel { get { if ((Status == BUS_STATUS.NEEDS_REFUEL) || (Status == BUS_STATUS.READY_FOR_TRAVEL)) return true; return false; } set { if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs("NeedsRefuel")); } } }
 
+        /// <summary>
+        /// the status of the bus situation
+        /// </summary>
         private BUS_STATUS status;
         public BUS_STATUS Status { get { return status; } set { status = value; if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs("Status")); } } }
 
-        public enum BUS_STATUS
+        public enum BUS_STATUS          //enum for the bus status
         {
             READY_FOR_TRAVEL, NEEDS_REFUEL, DANGEROUS, AT_TRAVEL, AT_TREATMENT, AT_REFUEL
         }
 
-        private string statusColor;
+        /// <summary>
+        /// update the color status of the bus
+        /// </summary>
+        private string statusColor;     
         public string StatusColor { get { return statusColor; } set { statusColor = value; if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs("StatusColor")); } } }
 
-        public void Update_Status()
-        {
+        public void Update_Status()         //function update the status of the bus after is change
+
+        {       //in case the bus did a treatment so update is status for redy to travel
             if (MileageSinceLastTreat < 20000 && lastTreatmentDate.AddYears(1).CompareTo(MainWindow.useMyRunningDate) > 0 && KMLeftToTravel > 0)
             {
-                Status = BUS_STATUS.READY_FOR_TRAVEL;
-                StatusColor = "LightGreen";
+                Status = BUS_STATUS.READY_FOR_TRAVEL;       //update status
+                StatusColor = "LightGreen";                 //update status color
                 // For the is_enabled buttons:
                 IsReady = true;
                 NeedsRefuel = true;
                 NeedsTreatment = false;
             }
+            //in case the bus need a treatment so update is status
             else if (MileageSinceLastTreat >= 20000 || lastTreatmentDate.AddYears(1).CompareTo(MainWindow.useMyRunningDate) <= 0)
             {
-                Status = BUS_STATUS.DANGEROUS;
-                StatusColor = "OrangeRed";
+                Status = BUS_STATUS.DANGEROUS;      //update status
+                StatusColor = "OrangeRed";          //update status color 
+                // For the is_enabled buttons:
                 NeedsTreatment = true;
                 IsReady = false;
                 NeedsRefuel = false;
             }
+            //in case the bus need a refuel 
             else if (KMLeftToTravel <= 0)
             {
                 Status = BUS_STATUS.NEEDS_REFUEL;
@@ -128,6 +138,7 @@ namespace dotNet5781_03B_0933_8558
 
         /////////////////////////////// Dates: ///////////////////////////////
 
+        //for showing the date short
         public string AbsorptionShortDate
         {
             get { return dateOfAbsorption.ToShortDateString(); }
@@ -155,6 +166,7 @@ namespace dotNet5781_03B_0933_8558
             }
         }
 
+        //return the short date of the last treatment
         public string TreatmentShortDate
         {
             get { return lastTreatmentDate.ToShortDateString(); }
@@ -182,7 +194,7 @@ namespace dotNet5781_03B_0933_8558
                 if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs("LastTreatmentDate")); }
             }
         }
-
+        //return the days until next treatment in a short model
         private int daysUntilNextTreat;
         public int DaysUntilNextTreat
         {
@@ -215,6 +227,7 @@ namespace dotNet5781_03B_0933_8558
         /// </summary>
         public double MileageAtLastTreat
         {
+            //return the mileage at last trearment rounding in to digits after the point
             get { return Math.Round(mileageAtLastTreat,2); }
             set
             {
@@ -233,7 +246,7 @@ namespace dotNet5781_03B_0933_8558
                 if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs("MileageSinceLastTreat")); }
             }
         }
-
+        //set and get for thr km to next treatmernt
         public double KMtoNextTreat
         {
             get { if (20000 - MileageSinceLastTreat >= 0) return Math.Round(20000 - MileageSinceLastTreat, 2); return 0; }
@@ -268,26 +281,27 @@ namespace dotNet5781_03B_0933_8558
             }
         }
 
-        private BackgroundWorker refuelWorker;
+        private BackgroundWorker refuelWorker;  //create a background worker for the refule
         /// <summary>
         /// The function restarts the kmLeftToRide with 1200 km
         /// </summary>
-        public void Refuel()
+        public void Refuel()            //function fo refuel the buses
         {
-            refuelWorker = new BackgroundWorker();
-            double part = (1200 - kmLeftToTravel) / 120;
+            refuelWorker = new BackgroundWorker();              //initialize the background worker
+            double part = (1200 - kmLeftToTravel) / 120;        // creatig part of km to add in the progress bar
 
-            refuelWorker.WorkerReportsProgress = true;
+            refuelWorker.WorkerReportsProgress = true;          
 
-            refuelWorker.ProgressChanged += (sender, args) =>
+            refuelWorker.ProgressChanged += (sender, args) =>   //the progress changed function
             {
-                KMLeftToTravel += part;
+                KMLeftToTravel += part;                         //add fuel to the bus in parts
             };
 
-            refuelWorker.DoWork += (sender, args) =>
+            refuelWorker.DoWork += (sender, args) =>            //the DoWork function 
             {
-                Status = BUS_STATUS.AT_REFUEL;
+                Status = BUS_STATUS.AT_REFUEL;                  //update status "at refule" as long the bus is stiil fueling
                 StatusColor = "OrangeRed";
+                //disabled the controlers until the refuel is done
                 IsReady = false;
                 NeedsRefuel = false;
                 NeedsTreatment = false;
@@ -299,16 +313,16 @@ namespace dotNet5781_03B_0933_8558
                     }
                     else
                     {
-                        refuelWorker.ReportProgress(0);
-                        try { Thread.Sleep(100); } catch (Exception) { }
+                        refuelWorker.ReportProgress(0);                     //report progress
+                        try { Thread.Sleep(100); } catch (Exception) { }    //sleep 100 milliseconds 120 times (for 2 hours)
                     }
                 }
             };
 
-            if (refuelWorker.IsBusy != true)
+            if (refuelWorker.IsBusy != true)                        //in case the background worker not busy
                 refuelWorker.RunWorkerAsync();
 
-            refuelWorker.RunWorkerCompleted += (sender, args) =>
+            refuelWorker.RunWorkerCompleted += (sender, args) =>        //the Run Worker Completed function
             {
                 if (args.Cancelled == true)
                 {
@@ -322,21 +336,22 @@ namespace dotNet5781_03B_0933_8558
             };
         }
 
-        private BackgroundWorker treatmentWorker;
+        private BackgroundWorker treatmentWorker;       //create a background worker for the treatment
 
-        public void Treatment()
+        public void Treatment()                         //function for background worker treatment
         {
-            treatmentWorker = new BackgroundWorker();
+            treatmentWorker = new BackgroundWorker();           //initialize the background worker
             treatmentWorker.WorkerReportsProgress = true;
 
-            treatmentWorker.ProgressChanged += (sender, args) =>
+            treatmentWorker.ProgressChanged += (sender, args) =>    //the progress changed function
             {
-                WorkEndsIn = 100 * args.ProgressPercentage / 1440;
+                WorkEndsIn = 100 * args.ProgressPercentage / 1440;  //treatment takes 24 hours
             };
 
-            treatmentWorker.DoWork += (sender, args) =>
+            treatmentWorker.DoWork += (sender, args) =>         //do work function
             {
-                Status = BUS_STATUS.AT_TREATMENT;
+                Status = BUS_STATUS.AT_TREATMENT;               //update status to "at treatment" as long the bus is in treatment
+                //disabled the controlers until the treatment is done
                 IsReady = false;
                 NeedsRefuel = false;
                 NeedsTreatment = false;
@@ -348,66 +363,64 @@ namespace dotNet5781_03B_0933_8558
                 {
                     for (int i = 1; i < 1441; i++) // 24 hours = 1440 minutes
                     {
-                        try { Thread.Sleep(100); } catch (Exception) { }
+                        try { Thread.Sleep(100); } catch (Exception) { }        //sleeps 100 miliseconds for 1441 times(24 hours)
                         treatmentWorker.ReportProgress(i);
                     }
                 }
             };
 
-            if (treatmentWorker.IsBusy != true)
+            if (treatmentWorker.IsBusy != true)             //in case worker is busy
                 treatmentWorker.RunWorkerAsync();
 
-            treatmentWorker.RunWorkerCompleted += (sender, args) =>
+            treatmentWorker.RunWorkerCompleted += (sender, args) =>     //Run Worker Completed function
             {
                 if (args.Cancelled == true)
                 {
 
                 }
                 else
-                {
-                    ////if (this.KMLeftToTravel < 1200)
-                    ////    Refuel();
-                    //else
-                        
-                    MileageAtLastTreat = Mileage;
-                    LastTreatmentDate = MainWindow.useMyRunningDate;
-                    DaysUntilNextTreat = 365;
+                {                                     
+                    MileageAtLastTreat = Mileage;                           //update mileage after treatment
+                    LastTreatmentDate = MainWindow.useMyRunningDate;       //update date
+                    DaysUntilNextTreat = 365;                               //update date until next treatment
                     WorkEndsIn = 0;
-                    if (this.KMLeftToTravel < 1200)
+                    if (this.KMLeftToTravel < 1200)                         //in case the bus needs a refuel also
                         Refuel();
                     else
-                        Update_Status();
+                        Update_Status();                                    //update status of the bus
                 }
             };
         }
 
-        private BackgroundWorker travelWorker;
+        private BackgroundWorker travelWorker;                      //create a background worker for the travel
 
-        public void Travel(double travel, double travelTime)
+        public void Travel(double travel, double travelTime)        //function for taking bus to a ride
         {
-            travelWorker = new BackgroundWorker();
-            travelWorker.WorkerReportsProgress = true;
-            double prev1 = Mileage;          
+            travelWorker = new BackgroundWorker();                  //initialize the background worker
+            travelWorker.WorkerReportsProgress = true;              //allow background worker to work
+            //keep the details of the bus for update after the ride
+            double prev1 = Mileage;                             
             double prev2 = MileageSinceLastTreat;
             double prev3 = KMLeftToTravel;
 
-            double minutes = (travel / travelTime) * 60;
-            double part = travel / minutes;
-            double integer = Math.Floor(minutes);
+            double minutes = (travel / travelTime) * 60;            //calculate the total minutes of the ride
+            double part = travel / minutes;                         //divide the minutes for parts to add in the progras changed func
+            double integer = Math.Floor(minutes);                   //the time that the worker need to work
 
-            travelWorker.ProgressChanged += (sender, args) =>
+            travelWorker.ProgressChanged += (sender, args) =>   //Progress Changed function    
             {
-                Mileage += part;
-                MileageSinceLastTreat += part;
-                if (KMLeftToTravel - part >= 0)
-                    KMLeftToTravel -= part;
+                Mileage += part;                                //add part to the milage
+                MileageSinceLastTreat += part;                  //add part to the milage since last treatment
+                if (KMLeftToTravel - part >= 0)                 //in case the bus have enough km to ride
+                    KMLeftToTravel -= part;                     //reduce the km left to travel of the bus
                 WorkEndsIn = (int)(100 * args.ProgressPercentage / integer);
             };
 
-            travelWorker.DoWork += (sender, args) =>
+            travelWorker.DoWork += (sender, args) =>                //do work function
             {
-                Status = BUS_STATUS.AT_TRAVEL;
+                Status = BUS_STATUS.AT_TRAVEL;                      //update bus status for "at travel"
                 StatusColor = "OrangeRed";
+                //disabled the controlers until the ride is done
                 IsReady = false;
                 NeedsTreatment = false;
                 NeedsRefuel = false;
@@ -417,28 +430,29 @@ namespace dotNet5781_03B_0933_8558
                 }
                 else
                 {
-                    for (int i = 1; i < integer + 1; i++)
+                    for (int i = 1; i < integer + 1; i++)           
                     {
-                        try { Thread.Sleep(100); } catch (Exception) { }
+                        try { Thread.Sleep(100); } catch (Exception) { }    //sleeps 100 miliseconds until the ride is done (as long as the minutes of the ride)
                         travelWorker.ReportProgress(i);
                     }
                 }
             };
 
-            if (travelWorker.IsBusy != true)
+            if (travelWorker.IsBusy != true)                            //in case worker is busy
                 travelWorker.RunWorkerAsync();
 
-            travelWorker.RunWorkerCompleted += (sender, args) =>
+            travelWorker.RunWorkerCompleted += (sender, args) =>    //Run Worker Completed function
             {
                 if (args.Cancelled == true)
                 {
 
                 }
                 else
+                //update the bus
                 {
-                    Mileage = prev1 + travel;
-                    MileageSinceLastTreat = prev2 + travel;
-                    KMLeftToTravel = prev3 - travel;
+                    Mileage = prev1 + travel;                       //update the mileage
+                    MileageSinceLastTreat = prev2 + travel;         //update the mileage since last treatment
+                    KMLeftToTravel = prev3 - travel;                //update the km left to travel
                     WorkEndsIn = 0;
                     Update_Status();          
                 }
--- dotNet5781_03B_0933_8558/Enums.cs
+++ dotNet5781_03B_0933_8558/Enums.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace dotNet5781_01_0933_8558
 {
-
+    //enum for the bus status
     public enum BUS_STATUS
     {
         READY_FOR_TRAVEL, AT_TRAVEL, AT_TREATMENT, AT_REFUEL // מוכן לנסיעה, באמצע נסיעה, בתדלוק, בטיפול.
