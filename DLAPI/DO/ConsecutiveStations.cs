8e3fe9773473b16abbe7e835237301e7ead1b26c
mendi2770 <73079004+mendi2770@users.noreply.github.com>
2021-01-13 13:13:04 +0200

    found solutain

--- DLAPI/DO/ConsecutiveStations.cs
+++ DLAPI/DO/ConsecutiveStations.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 using DalApi;
 namespace DO
 {
@@ -12,14 +15,29 @@ namespace DO
         public int BusStopKeyB { get; set; }   // Entity Key B
         public double Distance { get; set; }
         public TimeSpan TravelTime { get; set; }
+        //private TimeSpan travelTime;
+
+        //[XmlIgnore]
+        //public TimeSpan TravelTime
+        //{
+        //    get { return travelTime; }
+        //    set { travelTime = value; }
+        //}
+        //[XmlElement("TravelTime", DataType = "duration")]
+        //[DefaultValue("PT10M")]
+        //public string XmlTime
+        //{
+        //    get { return XmlConvert.ToString(travelTime); }
+        //    set { travelTime = XmlConvert.ToTimeSpan(value); }
+        //}
         public bool ObjectActive { get; set; }
-        /// <summary>
-        /// Formats a string which represents the Bus object
-        /// </summary>
-        /// <returns> Returns the string to print the object </returns>
-        public override string ToString()
-        {
-            return string.Format("Bus Stop Key A= {0}, Bus Stop Key B= {1}, Distance = {2}, TravelTime = {3}", BusStopKeyA, BusStopKeyB, Distance, TravelTime);
-        }
+/// <summary>
+/// Formats a string which represents the Bus object
+/// </summary>
+/// <returns> Returns the string to print the object </returns>
+public override string ToString()
+{
+    return string.Format("Bus Stop Key A= {0}, Bus Stop Key B= {1}, Distance = {2}, TravelTime = {3}", BusStopKeyA, BusStopKeyB, Distance, TravelTime);
+}
     }
 }
